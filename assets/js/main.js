8f4495363f116003067952b26edc15281df3d32e
Tom Faulhaber <faulhat@wwu.edu>
2024-12-17 17:55:06 -0800

    Added Sakura theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,57 @@
+const root = document.querySelector(":root");
+const faviconLink = document.querySelector("link[rel*='icon']");
+const arms = document.getElementById("arms");
+
+const themes = {
+   "coder": {
+      "vars": {
+         "main-bg-color": "#002b36",
+         "main-fg-color": "lightgreen",
+         "link-color": "crimson",
+         "code-bg-color": "#000040",
+         "code-fg-color": "white"
+      },
+      "icon": "arms.png"
+   },
+   "sakura": {
+      "vars": {
+         "main-bg-color": "#000020",
+         "main-fg-color": "#f67280",
+         "link-color": "#5efb6e",
+         "code-bg-color": "black",
+         "code-fg-color": "white"
+      },
+      "icon": "arms_sakura.png"
+   }
+};
+
+function setIcon(imageName) {
+   var imageURL = "/assets/images/" + imageName;
+   faviconLink.href = imageURL;
+   arms.src = imageURL;
+}
+
+function setColors(themeName) {
+   var theme = themes[themeName];
+   for (const [key, value] of Object.entries(theme["vars"])) {
+      root.style.setProperty("--" + key, value);
+   }
+
+   setIcon(theme["icon"]);
+   localStorage.setItem("theme", themeName);
+}
+
+const themeSelector = document.getElementById("themeSelector");
+
+const storedTheme = localStorage.getItem("theme");
+if (storedTheme === null) {
+   setColors("coder");
+} else {
+   setColors(storedTheme);
+   themeSelector.value = storedTheme;
+}
+
 const selected = document.getElementsByClassName("selected");
 for (let element of selected) {
    element.scrollIntoView({ "block": "nearest" });
 }
-
