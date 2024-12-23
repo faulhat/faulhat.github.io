b8601dac55c7d4306ac75c1b3a107df12178e064
Tom Faulhaber <faulhat@wwu.edu>
2024-12-23 13:06:46 -0800

    Added even more color themes

--- assets/js/themeselect.js
+++ assets/js/themeselect.js
@@ -3,7 +3,7 @@ const faviconLink = document.querySelector("link[rel*='icon']");
 const arms = document.getElementById("arms");
 
 const themes = {
-   "coder": {
+   "Coder": {
       "vars": {
          "main-bg-color": "#002b36",
          "main-fg-color": "lightgreen",
@@ -13,7 +13,7 @@ const themes = {
       },
       "icon": "arms.png"
    },
-   "sakura": {
+   "Sakura": {
       "vars": {
          "main-bg-color": "#000020",
          "main-fg-color": "#f67280",
@@ -23,7 +23,7 @@ const themes = {
       },
       "icon": "arms_sakura.png"
    },
-   "vampiro": {
+   "Vampiro": {
       "vars": {
          "main-bg-color": "#000020",
          "main-fg-color": "#990012",
@@ -33,7 +33,7 @@ const themes = {
       },
       "icon": "arms_vampiro.png"
    },
-   "power": {
+   "Power": {
       "vars": {
          "main-bg-color": "#012456",
          "main-fg-color": "#61d6d6",
@@ -42,6 +42,46 @@ const themes = {
          "code-fg-color": "white"
       },
       icon: "arms_power.png"
+   },
+   "Kaffee": {
+        "vars": {
+            "main-bg-color": "#f5deb3",
+            "main-fg-color": "#8b4513",
+            "link-color": "#800000",
+            "code-bg-color": "black",
+            "code-fg-color": "white"
+        },
+        icon: "arms_kaffee.png"
+   },
+   "Seaglass": {
+        "vars": {
+            "main-bg-color": "#2f4f4f",
+            "main-fg-color": "#00fa9a",
+            "link-color": "#f08080",
+            "code-bg-color": "black",
+            "code-fg-color": "white"
+        },
+        icon: "arms_seaglass.png"
+   },
+   "Neo": {
+        "vars": {
+            "main-bg-color": "black",
+            "main-fg-color": "cyan",
+            "link-color": "red",
+            "code-bg-color": "indigo",
+            "code-fg-color": "#ffdab9"
+        },
+        icon: "arms_neo.png"
+   },
+   "Mono": {
+        "vars": {
+            "main-bg-color": "black",
+            "main-fg-color": "#a9a9a9",
+            "link-color": "white",
+            "code-bg-color": "#d3d3d3",
+            "code-fg-color": "black"
+        },
+        icon: "arms_mono.png"
    }
 };
 
@@ -63,9 +103,13 @@ function setColors(themeName) {
 
 const themeSelector = document.getElementById("themeSelector");
 
+for (const key of Object.keys(themes)) {
+    themeSelector.add(new Option(key, key));
+}
+
 const storedTheme = localStorage.getItem("theme");
 if (storedTheme === null) {
-   setColors("coder");
+   setColors("Coder");
 } else {
    setColors(storedTheme);
    themeSelector.value = storedTheme;
